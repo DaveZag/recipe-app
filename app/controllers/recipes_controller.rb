d06957479f7888f0c344da2de15af0605407cd53
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-15 20:14:40 +0200

    Fix linters errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -27,7 +27,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
-    
+
     respond_to do |format|
       if @recipe.save
         format.html { redirect_to recipes_url, notice: 'Recipe was successfully created.' }
