ee6e5d59c6d21f66b466d0a6c3e6c8ca771da346
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-02-18 21:23:36 +0200

    Fix bugs in popup window

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
+
+// temporary solution to turbo links event listener disappearance when changing views without reloading
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.session.drive = false;
